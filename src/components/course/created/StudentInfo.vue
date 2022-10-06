81612a61851a4d45106535b12d2df1ddb24396d4
YunJia <sanchuanguli@163.com>
2022-10-06 08:55:08 +0800

    添加了简单的学生列表的显示, 还有一堆问题.

--- src/components/course/created/StudentInfo.vue
+++ src/components/course/created/StudentInfo.vue
@@ -1,12 +1,35 @@
 <template>
-  <panda-men>学生</panda-men>
+  <el-card>
+    <el-table :data="studentList" style="width: 100%">
+      <el-table-column label="头像"></el-table-column>
+      <el-table-column prop="clazz" label="班级"></el-table-column>
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="number" label="学号"></el-table-column>
+      <el-table-column prop="phone" label="手机号"></el-table-column>
+    </el-table>
+    <br/>
+    <span style="color: red">还差一个成绩, 差一个操作, 差一个头像显示, 差分页, 差排序, 差搜索</span>
+  </el-card>
 </template>
 
 <script>
-  import PandaMen from "@/components/PandaMen";
   export default {
     name: "StudentInfo",
-    components: {PandaMen}
+    data() {
+      return {
+        courseId: '',
+        studentList: [],
+      }
+    },
+    created() {
+      this.courseId = this.$route.query.courseId;
+      this.service.get("select/student-list/" + this.courseId)
+          .then(res => {
+            console.log(res);
+            this.studentList = res.data;
+          })
+    }
+    // TODO 还差一个成绩, 差一个操作, 差一个头像显示, 差分页, 差排序, 差搜索
   }
 </script>
 
